5d38c01791da7cc504d75b7086cd7aae77092758
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-19 22:19:46 +0300

    add getMessages controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,6 @@
+import User from '../models/user.model.js';
+import Message from '../models/user.model.js';
+
 export const getUsersForSidebar = async (req, res) => {
   try {
     const loggedInUserId = req.user._id;
@@ -7,5 +10,24 @@ export const getUsersForSidebar = async (req, res) => {
   } catch (error) {
     console.log('Error in getUsersForSidebar: ', error.message);
     res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log('Error in getMessages controller:', error.message);
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getUsersForSidebar } from '../controllers/message.controller.js';
+import { getUsersForSidebar, getMessages } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 router.get('/users', protectRoute, getUsersForSidebar);
+router.get('/:id', protectRoute, getMessages);
 
 export default router;
