62eeb91479839fdf5f8c3c3eca7b81ba7470fbf9
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-19 21:18:27 +0300

    add checkAuth controller

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
+import { login, logout, signup, updateProfile, checkAuth } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.post('/login', login);
 router.post('/logout', logout);
 
 router.put('/update-profile', protectRoute, updateProfile);
+
+router.get('/check', protectRoute, checkAuth);
 
 export default router;
