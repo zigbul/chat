d13e4f9f83a19231d885c93091644a53b691583e
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-19 20:16:04 +0300

    add auth middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup, updateProfile } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
+router.put('/update-profile', protectRoute, updateProfile);
 
 export default router;
