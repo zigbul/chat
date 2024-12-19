a4fdb56d218d099910d9a5a4e277a4741afdb603
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-19 21:09:34 +0300

    add update profile controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -83,4 +84,27 @@ export const logout = (req, res) => {
   }
 };
 
-export const updateProfile = async (req, res) => {};
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+      return res.status(400).json({ message: 'Profile pic is required' });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        profilePic: uploadResponse.secure_url,
+      },
+      { new: true },
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log('Error in update profile', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
+
 router.put('/update-profile', protectRoute, updateProfile);
 
 export default router;
