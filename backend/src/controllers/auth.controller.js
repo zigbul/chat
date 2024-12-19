777390d7df6b9ebadbf1d7dd522a75f921b920b7
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-19 19:58:37 +0300

    update auth logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -74,5 +74,13 @@ export const login = async (req, res) => {
 };
 
 export const logout = (req, res) => {
-  res.send('logout route');
+  try {
+    res.cookie('jwt', '', { maxAge: 0 });
+    res.status(200).json({ message: 'Logged out successfully' });
+  } catch (error) {
+    console.log('Error in logout controller', error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 };
+
+export const updateProfile = async (req, res) => {};
