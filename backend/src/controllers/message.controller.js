d409d6811329402aecc0bfd10582d62d0fa50b95
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-19 22:30:02 +0300

    add sendMessage controller

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import Message from '../models/user.model.js';
+import cloudinary from '../lib/cloudinary.js';
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -28,6 +29,35 @@ export const getMessages = async (req, res) => {
     res.status(200).json(messages);
   } catch (error) {
     console.log('Error in getMessages controller:', error.message);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log('Error in sendMessage controller:', error.message);
     res.status(500).json({ message: 'Internal server error' });
   }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getUsersForSidebar, getMessages } from '../controllers/message.controller.js';
+import { getUsersForSidebar, getMessages, sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
 router.get('/users', protectRoute, getUsersForSidebar);
 router.get('/:id', protectRoute, getMessages);
+
+router.post('/send/:id', protectRoute, sendMessage);
 
 export default router;
