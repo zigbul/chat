e20b539467cf461fa111a4090dfd164e5ed763fc
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-27 01:01:21 +0300

    update login page and add login function

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -35,6 +35,20 @@ export const useAuthStore = create((set) => ({
     }
   },
 
+  login: async (data) => {
+    set({ isLoggingIn: true });
+
+    try {
+      const res = await axiosInstance.post('/auth/login', data);
+      set({ authUser: res.data });
+      toast.success('Logged in successfully');
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
   logout: async () => {
     try {
       await axiosInstance.post('/auth/logout');
