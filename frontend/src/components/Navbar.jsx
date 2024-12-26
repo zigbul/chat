900a98c5011f44f4c30792023da7fd2ebdf0f29b
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-26 23:54:41 +0300

    update navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,48 @@
+import { Link } from 'react-router-dom';
+import { MessageSquare, Settings, User, LogOut } from 'lucide-react';
+
+import { useAuthStore } from '../store/useAuthStore.js';
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { logout, authUser } = useAuthStore();
+
+  return (
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 backdrop-blur-lg bg-base-100/80">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
+              <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <MessageSquare className="size-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold">Chat</h1>
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Link to={'/settings'} className={`btn btn-sm gap-2 transition-colors`}>
+              <Settings className="size-4" />
+              <span className="hidden sm:inline">Settings</span>
+            </Link>
+
+            {authUser && (
+              <>
+                <Link to={'/ptofile'} className={`btn btn-sm gap-2`}>
+                  <User className="size-5" />
+                  <span className="hidden sm:inline">Profile</span>
+                </Link>
+
+                <button className="flex gap-2 items-center" onClick={logout}>
+                  <LogOut className="size-5" />
+                  <span className="hidden sm:inline">Logout</span>
+                </button>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
