8ac4b03a392d6c8b3ba2c064817ed10740272f1c
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-27 13:15:59 +0300

    add updateProfile function in store

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -59,5 +59,18 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  updateProfile: async (data) => {},
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+
+    try {
+      const res = await axiosInstance.put('/auth/update-profile', data);
+      set({ authUser: res.data });
+      toast.success('Profile updated successfully');
+    } catch (error) {
+      console.log('Error in update profile:', error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
