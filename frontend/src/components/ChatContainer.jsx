97db45d3a985c61eb4ae4d0fefc08a4940bb6e9b
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-27 19:28:27 +0300

    add chat header in chat container

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,5 +1,28 @@
+import { useEffect } from 'react';
+
+import { useChatStore } from '../store/useChatStore.js';
+
+import ChatHeader from './ChatHeader.jsx';
+import MessageInput from './MessageInput.jsx';
+
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessagesLoading) return <div>Loading...</div>;
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
+  );
 };
 
 export default ChatContainer;
---
+++ frontend/src/components/ChatHeader.jsx
@@ -0,0 +1,36 @@
+import { X } from 'lucide-react';
+
+import { useChatStore } from '../store/useChatStore.js';
+import { useAuthStore } from '../store/useAuthStore.js';
+
+const ChatHeader = () => {
+  const { selectedUser, setSelectedUser } = useChatStore();
+  const { onlineUsers } = useAuthStore();
+
+  return (
+    <div className="p-2.5 border-b border-base-300">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <div className="avatar">
+            <div className="size-10 rounded-full relative">
+              <img src={selectedUser.profilePic || '/avatar.png'} alt={selectedUser.fullName} />
+            </div>
+          </div>
+
+          <div>
+            <h3 className="font-medium">{selectedUser.fullName}</h3>
+            <p className="text-sm text-base-content/70">
+              {onlineUsers.includes(selectedUser._id) ? 'Online' : 'Offline'}
+            </p>
+          </div>
+        </div>
+
+        <button onClick={() => setSelectedUser(null)}>
+          <X />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
