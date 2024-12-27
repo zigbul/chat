a07d22e54cda848311e9213c3eaabfaa66126871
zigbul <65498093+zigbul@users.noreply.github.com>
2024-12-27 15:14:56 +0300

    updating homepage, add NoChatSelected component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,25 @@
+import { useChatStore } from '../store/useChatStore.js';
+
+import Sidebar from '../components/Sidebar.jsx';
+import NoChatSelected from '../components/NoChatSelected.jsx';
+import ChatContainer from '../components/ChatContainer.jsx';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { selectedUser } = useChatStore();
+
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex item-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />
+
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
