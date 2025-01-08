25ab0b0ae68f99ed5c9a39c2ab6cacdcbe8b0213
zigbul <65498093+zigbul@users.noreply.github.com>
2025-01-08 18:58:08 +0300

    fix development routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+import path from 'path';
+
 import { connectDB } from './lib/db.js';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -11,6 +13,7 @@ import { app, server } from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +21,14 @@ app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE.ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from '../lib/axios.js';
 import { toast } from 'react-hot-toast';
 import { io } from 'socket.io-client';
 
-const BASE_URL = 'http://localhost:5001';
+const BASE_URL = import.meta.env.Mode === 'development' ? 'http://localhost:5001' : '/';
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
